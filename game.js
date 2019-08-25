78783aa7df714065c2c06330de1d4764c8061a2b
Moin Khan <kmoin786@gmail.com>
2019-08-25 12:43:20 +0530

    added user click function to boxes

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 function nextSequence(){
     var randomNumber = Math.round(Math.random()*3);
     var randomChosenColour = buttonColours[randomNumber];
@@ -7,6 +8,17 @@ function nextSequence(){
     $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
     var sound = new Audio(`./sounds/${randomChosenColour}.mp3`);
     sound.play();
+    $("div.btn").click(function(){
+            var userChosenColour = $(this).attr('id');
+            userClickedPattern.push(userChosenColour);
+            playSound(userChosenColour);
+            
+        }
+    );
+};
+function playSound(name){
+    var usersound = new Audio(`./sounds/${name}.mp3`);
+    usersound.play();
 };
 nextSequence();
 
